67703aed41a04e704784273434cdc40a032816d0
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2022-04-13 14:07:18 -0700

    intial

---
+++ directory.py
@@ -0,0 +1,37 @@
+import os
+from operator import itemgetter
+import requests
+import json
+
+authorization_service_url = os.getenv("AUTHORIZER_SERVICE_URL")
+api_key = os.getenv("AUTHORIZER_API_KEY")
+tenant_id = os.getenv("TENANT_ID")
+
+def resolve_user_by_identity(sub):
+    response = requests.post(
+        authorization_service_url + "/api/v1/dir/identities",
+        headers={
+            "Authorization": "basic " + api_key,
+            "aserto-tenant-id": tenant_id,
+            "Content-Type": "application/json"
+        },
+        data = json.dumps({
+            "identity": sub
+        })
+    )
+
+    json_response = response.json()
+    return itemgetter('id')(json_response)
+
+def resolve_user_by_user_id(user_id):
+    response = requests.get(
+        authorization_service_url + "/api/v1/dir/users/" + user_id + "?fields.mask=id,display_name,picture,email",
+        headers={
+            "Authorization": "basic " + api_key,
+            "aserto-tenant-id": tenant_id,
+            "Content-Type": "application/json"
+        }
+    )
+
+    json_response = response.json()
+    return itemgetter('result')(json_response)
---
+++ server.py
@@ -0,0 +1,71 @@
+
+from flask import Flask, g, jsonify, request, abort, Response
+from flask_cors import CORS
+import sqlite3
+import os
+from aserto_idp.oidc import identity_provider
+from .aserto_options import load_aserto_options_from_environment
+from .directory import resolve_user_by_identity, resolve_user_by_user_id
+from .db import list_todos, insert_todo, update_todo, delete_todo
+from jose.exceptions import JWTError
+from flask_aserto import AsertoMiddleware, AuthorizationError
+
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+CORS(app, headers=["Content-Type", "Authorization"])
+
+aserto_options = load_aserto_options_from_environment()
+aserto = AsertoMiddleware(**aserto_options)
+
+@app.errorhandler(AuthorizationError)
+def authorization_error(e):
+    return "Authorization Error", 403
+
+@app.route('/todos', methods=['GET'])
+@aserto.authorize
+def get_todos():
+    results = list_todos()
+    return jsonify(results)
+
+@app.route('/todo', methods=['POST'])
+@aserto.authorize
+def post_todo():
+    todo = request.get_json()
+    insert_todo(todo)
+    return jsonify(todo)
+
+
+@app.route('/todo/<oid>', methods=['PUT'])
+@aserto.authorize
+def put_todo(oid):
+    todo = request.get_json()
+    update_todo(todo)
+    return todo
+
+@app.route('/todo/<oid>', methods=['DELETE'])
+@aserto.authorize
+def remove_todo(oid):
+    todo = request.get_json()
+    delete_todo(todo)
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
+
+@app.route('/user/<uid>', methods=['GET'])
+@aserto.authorize
+def get_user(uid):
+    uid = resolve_user_by_identity(uid)
+    user = resolve_user_by_user_id(uid)
+    return jsonify(user)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=3001, debug=True)
